9b3460a5c570b3fd5bf8cc27472474aeaaaee307
Kayden <86061794+Kaydenvn@users.noreply.github.com>
2024-04-01 10:38:02 +0700

    fix

--- src/layouts/MainLayout/Mainlayout.tsx
+++ src/layouts/MainLayout/Mainlayout.tsx
@@ -10,22 +10,7 @@ export default function Mainlayout({
     <div className="relative w-full bg-white">
       <Header />
       <div>{children}</div>
-      <Footer
-        colorBgContainer="white"
-        style={{
-          position: "fixed",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          zIndex: 1,
-          height: "var(--footer-height)",
-          textAlign: "center",
-          lineHeight: "var(--footer-height)",
-          fontSize: "12px",
-          padding: "0",
-        }}
-        className="border-t border-gray-200"
-      />
+      <Footer />
     </div>
   );
 }
