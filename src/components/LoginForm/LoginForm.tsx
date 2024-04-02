4ecc1aaec880b443338e00183d0a9ae4c788ee47
Kayden <86061794+Kaydenvn@users.noreply.github.com>
2024-04-03 01:06:55 +0700

    add form validation

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,7 +1,21 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import Logo from "src/assets/bk-logo.png";
 
+type FormDataType = {
+  email: string;
+  password: string;
+};
+
 export default function LoginForm() {
+  const form = useForm<FormDataType>();
+  const { register, handleSubmit, formState } = form;
+  const { errors } = formState;
+
+  const onSubmit = (data: FormDataType) => {
+    console.log(data);
+  };
+
   return (
     <div className="lg:w-2/6 md:w-1/2 bg-gray-50 rounded-lg p-8 flex flex-col md:ml-10 w-full mt-10 md:mt-0">
       <div className="mb-10">
@@ -13,29 +27,70 @@ export default function LoginForm() {
         </h2>
         <p className="text-center text-sm text-gray-600 mt-5">
           Bạn không có tài khoản? Hãy liên hệ{" "}
-          <Link to="#" className="font-medium text-primary hover:opacity-80">
+          <Link to="#" className="font-medium text-soft hover:opacity-80">
             quản trị viên
           </Link>
         </p>
       </div>
 
-      <div className="relative mb-4">
-        <label className="input input-bordered mb-7 flex items-center gap-2">
-          Email
-          <input
-            type="text"
-            className="grow"
-            placeholder="example@hcmut.edu.vn"
-          />
-        </label>
-        <label className="input input-bordered mb-7 flex items-center gap-2">
-          Password
-          <input type="password" className="grow" placeholder="********" />
-        </label>
-      </div>
-      <button className="text-white bg-soft border-0 py-2 px-8 focus:outline-none hover:opacity-80 rounded text-lg">
-        Button
-      </button>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+        className="relative mb-4"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="input input-bordered mb-2 flex items-center gap-2"
+          >
+            Email
+            <input
+              type="text"
+              id="email"
+              {...register("email", {
+                required: "Email không được để trống",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@hcmut.edu.vn$/i,
+                  message: "Email phải là email của trường HCMUT",
+                },
+              })}
+              className="grow"
+              placeholder="example@hcmut.edu.vn"
+            />
+          </label>
+          {errors.email && (
+            <p className="text-red-500 text-sm mb-4">{errors.email.message}</p>
+          )}
+        </div>
+
+        <div className="mb-4">
+          <label className="input input-bordered mb-1 flex items-center gap-2">
+            Password
+            <input
+              id="password"
+              {...register("password", {
+                required: "Password không được để trống",
+                minLength: {
+                  value: 6,
+                  message: "Password phải có ít nhất 6 ký tự",
+                },
+              })}
+              type="password"
+              className="grow"
+              placeholder="******"
+            />
+          </label>
+          {errors.password && (
+            <p className="text-red-500 text-sm mb-4">
+              {errors.password.message}
+            </p>
+          )}
+        </div>
+
+        <button className="text-white bg-soft border-0 py-2 px-8 w-full focus:outline-none hover:opacity-80 rounded text-lg">
+          Button
+        </button>
+      </form>
     </div>
   );
 }
