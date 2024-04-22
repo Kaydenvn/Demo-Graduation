97c126c09a275ee4c1c42ba692dc48daad369683
Kayden <86061794+Kaydenvn@users.noreply.github.com>
2024-04-23 01:53:24 +0700

    fix sidebar

--- src/components/Dashboard/Sidebar/Sidebar.tsx
+++ src/components/Dashboard/Sidebar/Sidebar.tsx
@@ -4,6 +4,7 @@ import {
   ProductOutlined,
   BookOutlined,
 } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
   return (
@@ -15,43 +16,43 @@ export default function Sidebar() {
       <div className="h-full px-3 pb-4 overflow-y-auto bg-white ">
         <ul className="space-y-2 font-medium">
           <li>
-            <a
-              href="/dashboard"
+            <Link
+              to="/dashboard"
               className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
             >
               <DashboardOutlined />
               <span className="ms-3">Quản lý</span>
-            </a>
+            </Link>
           </li>
 
           <li>
-            <a
-              href="/dashboard/users"
+            <Link
+              to="/dashboard/users"
               className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
             >
               <UserOutlined />
               <span className="ms-3">Người dùng </span>
-            </a>
+            </Link>
           </li>
 
           <li>
-            <a
-              href="/dashboard/models"
+            <Link
+              to="/dashboard/models"
               className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
             >
               <ProductOutlined />
               <span className="ms-3">Mô hình </span>
-            </a>
+            </Link>
           </li>
 
           <li>
-            <a
-              href="/dashboard/subjects"
+            <Link
+              to="/dashboard/subjects"
               className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
             >
               <BookOutlined />
               <span className="ms-3">Môn học </span>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
