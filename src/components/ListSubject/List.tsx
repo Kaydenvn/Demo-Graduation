c8264c3572bd40def175aab45e58e8d9682901cd
Kayden <86061794+Kaydenvn@users.noreply.github.com>
2024-05-04 16:34:31 +0700

    add subject

--- src/components/ListSubject/List.tsx
+++ src/components/ListSubject/List.tsx
@@ -1,3 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAllSubjects, getSubjectForThumbnail } from "src/api/Subject.api";
+
 const datasource = [
   {
     key: "1",
@@ -20,6 +23,11 @@ const datasource = [
 ];
 
 const ListSubject = () => {
+  const subjectQuery = useQuery({
+    queryKey: ["subject"],
+    queryFn: () => getSubjectForThumbnail(),
+  });
+
   return (
     <div className="container my-2 mx-auto ">
       <div className="text-2xl font-medium mb-8 px-2">Các Mô Hình</div>
@@ -27,52 +35,35 @@ const ListSubject = () => {
         Các mô hình trong xưởng C3
       </p>
       <div className="flex flex-wrap -mx-1 lg:-mx-4">
-        {datasource.map((item) => (
-          <div
-            className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3"
-            key={item.key}
-          >
-            <article className="overflow-hidden rounded-lg shadow-lg">
-              <a href="#">
-                <img
-                  alt="Placeholder"
-                  className="block h-auto w-full"
-                  src="https://picsum.photos/600/400/?random"
-                />
-              </a>
-              <header className="flex items-center justify-between leading-tight p-2 md:p-4">
-                <h1 className="text-lg">
-                  <a
-                    className="no-underline hover:underline text-black"
-                    href="#"
-                  >
-                    {item.name}
-                  </a>
-                </h1>
-              </header>
-              <footer className="flex items-center justify-between leading-none p-2 md:p-4">
-                <a
-                  className="flex items-center no-underline hover:underline text-black"
-                  href="#"
-                >
+        {subjectQuery.data.data.map(
+          (item: { key: string; name: string; photo: string }) => (
+            <div
+              // let list srollable
+              className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3"
+              key={item.key}
+            >
+              <article className="overflow-hidden rounded-lg shadow-lg">
+                <a href="#">
                   <img
                     alt="Placeholder"
-                    className="block rounded-full"
-                    src="https://picsum.photos/32/32/?random"
+                    className="block h-auto w-full"
+                    src={item.photo}
                   />
-                  <p className="ml-2 text-sm">{item.author}</p>
                 </a>
-                <a
-                  className="no-underline text-grey-darker hover:text-red-dark"
-                  href="#"
-                >
-                  <span className="hidden">Like</span>
-                  <i className="fa fa-heart" />
-                </a>
-              </footer>
-            </article>
-          </div>
-        ))}
+                <header className="flex items-center justify-between leading-tight p-2 md:p-4">
+                  <h1 className="text-lg">
+                    <a
+                      className="no-underline hover:underline text-black"
+                      href="#"
+                    >
+                      {item.name}
+                    </a>
+                  </h1>
+                </header>
+              </article>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
